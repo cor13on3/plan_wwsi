665825a7e370cfdcfff2b5497f34803fb8db48fe
Kamil Łoń <k.lon@raks.pl>
2021-05-24 16:52:43 +0200

    Aktualizacja bibliotek

--- Plan.mobile/PlanWWSI/Services/HTTP.cs
+++ Plan.mobile/PlanWWSI/Services/HTTP.cs
@@ -8,7 +8,8 @@ namespace PlanWWSI.Services
     public class HTTP
     {
         private HttpClient _httpClient;
-        private static string URL = "http://10.0.2.2:60211";
+        //private static string URL = "http://10.0.2.2:60211";
+        private static string URL = "http://192.168.0.129:60211";
 
         public HTTP()
         {
